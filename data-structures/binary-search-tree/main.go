3b1cac756daab560c42a1549ae75ffdde0ad11a0
Hamid Oujand <hmdrzadev@gmail.com>
2024-05-30 17:41:40 +0330

    tree traversal

--- data-structures/binary-search-tree/main.go
+++ data-structures/binary-search-tree/main.go
@@ -35,6 +35,28 @@ Big O:
 insertion: O(log n)
 searching: O(log n)
 
+
+Tree Traversal: is a way that we are going to visit each node at least 1 time.
+this can be apply of any kind of tree, binary ,binary search ...
+
+there are 2 common ways of traversing a tree:
+
+-Breadth first search: traversing across the tree horizontally, means visiting all nodes
+in the same level first then move down one layer.
+
+-depth first search: traversing down the tree vertically till end then come up and visit others
+
+
+when to use "DFS" vs "BSF":
+
+their "time complexity" is the same, so they differ in "space complexity"
+and that depends on the "tree" itself, for example if you have a "wide" tree
+using "bfs" on it requires a queue that is going to store a lot of data and grows
+in this case using "dfs" is better since the depth of the tree is not much compare to it's breadth.
+
+and if you have a tree which is "deep" and not that "wide", using "bfs" on it will take less space
+
+
 */
 
 type Node struct {
@@ -123,12 +145,121 @@ func (bst *BinarySearchTree) Find(val int) *Node {
 	}
 }
 
+// Iterative
+func (bst *BinarySearchTree) BFS() []int {
+	var results []int
+	var queue []*Node //queue to hold the visited nodes
+	//we add "root" into the queue
+	queue = append(queue, bst.root)
+
+	//now we loop till nothing inside of the queue
+	for len(queue) > 0 {
+		//we access the first from the queue
+		candidate := queue[0]
+		//put the value into result slice
+		results = append(results, candidate.val)
+		//check if there is a left or right
+		if candidate.left != nil {
+			queue = append(queue, candidate.left)
+		}
+		if candidate.right != nil {
+			queue = append(queue, candidate.right)
+		}
+		//now we dequeue the first from the queue
+		queue = queue[1:]
+	}
+
+	return results
+}
+
+// Recursive
+func (bst *BinarySearchTree) DFSPreOrder() []int {
+	var result []int
+	current := bst.root
+	traversePreOrder(current, &result)
+	return result
+}
+
+func (bst *BinarySearchTree) DFSPostOrder() []int {
+	var result []int
+	current := bst.root
+	traversePostOrder(current, &result)
+	return result
+}
+
+func (bst *BinarySearchTree) DFSInOrder() []int {
+	var result []int
+	current := bst.root
+	traverseInOrder(current, &result)
+	return result
+}
+
+func traversePreOrder(node *Node, result *[]int) {
+	//end of the branch
+	if node == nil {
+		return
+	}
+
+	//add it into result
+	*result = append(*result, node.val)
+
+	if node.left != nil {
+		//call it on the left side
+		traversePreOrder(node.left, result)
+	}
+	if node.right != nil {
+		traversePreOrder(node.right, result)
+	}
+}
+
+func traversePostOrder(node *Node, result *[]int) {
+	//end of the branch
+	if node == nil {
+		return
+	}
+
+	if node.left != nil {
+		//call it on the left side
+		traversePostOrder(node.left, result)
+	}
+	if node.right != nil {
+		traversePostOrder(node.right, result)
+	}
+
+	//add it into result after exploring it's left and right
+	*result = append(*result, node.val)
+}
+
+func traverseInOrder(node *Node, result *[]int) {
+	//end of the branch
+	if node == nil {
+		return
+	}
+
+	if node.left != nil {
+		//call it on the left side
+		traverseInOrder(node.left, result)
+	}
+
+	//add it into result after
+
+	*result = append(*result, node.val)
+
+	if node.right != nil {
+		traverseInOrder(node.right, result)
+	}
+}
+
 func main() {
 	bst := BinarySearchTree{}
-	bst.Insert(100)
-	bst.Insert(50)
-	bst.Insert(150)
-	bst.Insert(300)
-	bst.Insert(40)
-	fmt.Println(bst.Find(40))
+	bst.Insert(10)
+	bst.Insert(6)
+	bst.Insert(15)
+	bst.Insert(3)
+	bst.Insert(8)
+	bst.Insert(20)
+	fmt.Println(bst.BFS())
+	fmt.Println(bst.DFSPreOrder())
+	fmt.Println(bst.DFSPostOrder())
+	fmt.Println(bst.DFSInOrder())
 }
