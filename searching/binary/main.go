c3ab6de1f9a0a24dfbb67209744f9f1a53541c5c
Hamid Oujand <hmdrzadev@gmail.com>
2024-06-24 15:57:16 +0330

    binary search recursive

--- searching/binary/main.go
+++ searching/binary/main.go
@@ -16,8 +16,10 @@ Worst: O(log n) based on "n" as input it will take "log n" steps to run, example
 func main() {
 	items := []int{1, 2, 3, 10, 20, 24, 30, 32, 40, 50, 55, 59, 100}
 	fmt.Println(binarySearch(items, 30))
+	fmt.Println(binarySearchRecursive(items, 30))
 }
 
+// O(log n) Time | O(1) Space
 func binarySearch(items []int, target int) int {
 	left := 0
 	right := len(items) - 1
@@ -36,4 +38,27 @@ func binarySearch(items []int, target int) int {
 		}
 	}
 	return -1
+}
+
+// O(log n) Time | O(log n) Space
+func binarySearchRecursive(items []int, target int) int {
+	return recursive(items, target, 0, len(items)-1)
+}
+
+func recursive(items []int, target int, left int, right int) int {
+	//base case
+	if left > right {
+		return -1
+	}
+	//calculate middle
+	mid := (left + right) / 2
+	candidate := items[mid]
+	if candidate == target {
+		return mid
+	}
+	if candidate > target {
+		return recursive(items, target, left, mid-1)
+	} else {
+		return recursive(items, target, mid+1, right)
+	}
 }
